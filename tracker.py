aa0e48a2295bce1b2c294be095cb24f98537b964
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-13 22:51:58 -0400

    Features narrowed down in data set

--- tracker.py
+++ tracker.py
@@ -5,3 +5,4 @@ df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
+df = df['Adj. Clise', 'HL_PCT', 'PCT_Change', 'Adj. Volume']
