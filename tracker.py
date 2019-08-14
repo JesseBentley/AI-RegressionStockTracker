d2d66a326e34bcc0ae9c98c011548dcea9fa1571
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-13 23:08:39 -0400

    Pre-processing started

--- tracker.py
+++ tracker.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import quandl
-import math
+import quandl , math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -11,7 +13,11 @@ df = df['Adj. Clise', 'HL_PCT', 'PCT_Change', 'Adj. Volume']
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.1 * len(df)))
-
+forecast_out = int(math.ceil(0.01 * len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
 
