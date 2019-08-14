90bbaf8333538fba743a098f712dc82a6adf33a0
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-13 23:11:59 -0400

    Training data split completed

--- tracker.py
+++ tracker.py
@@ -15,9 +15,11 @@ df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01 * len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
+df.dropna(inplace = True)
 
 X = np.array(df.drop(['label'], 1))
 y = np.array(df['label'])
 X = preprocessing.scale(X)
+y = np.array(df['label'])
 
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
