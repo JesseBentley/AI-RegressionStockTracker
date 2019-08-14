68951087add6e753d7d91eb53c3dc7724157c0f2
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-14 09:16:46 -0400

    Progress on regression

--- tracker.py
+++ tracker.py
@@ -14,3 +14,20 @@ data = data[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
 data.fillna(-9999, inplace = True)
 forecast_out = int(math.ceil(0.01 * len(data)))
+
+data['label'] = data[forecast_col].shift(-forecast_out)
+
+X = np.array(data.drop('label'), 1)
+X = preprocessing.scale(X)
+X = X[: -forecast_out]
+X_lately = X[-forecast_out:]
+data.dropna(inplace = True)
+y = np.array(data['label'])
+y = np.array(data['label'])
+
+X_train, X_test, y_train, y_test = cross_validate.train_test_split(X, y, test_size = 0.2)
+clf = LinearRegression(n_jobs = -1)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(data.head)
