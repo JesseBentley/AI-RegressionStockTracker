f526bbfd719ad3fab03f24802a3780fa97c7cebb
Jesse Bentley <JesseDBentley@outlook.com>
2019-08-14 05:52:05 -0400

    Latested update - need to debug

--- tracker.py
+++ tracker.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import quandl , math
 import numpy as np
-from sklearn import preprocessing, cross_validation, svm
+from sklearn import preprocessing
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_validate
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
-df = df['Adj. Clise', 'HL_PCT', 'PCT_Change', 'Adj. Volume']
+df = df['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']
 
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
@@ -22,8 +23,8 @@ y = np.array(df['label'])
 X = preprocessing.scale(X)
 y = np.array(df['label'])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
-clf = LinearRegression(n_jobs = -1)
+X_train, X_test, y_train, y_test = cross_validate.train_test_split(X, y, test_size = 0.2)
+clf = LinearRegression(n_jobs = 2)
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
